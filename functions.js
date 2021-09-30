85a26e861ba2288b13c73018b2b694f049442316
leonardfrot <leonard.frote@he-arc.ch>
2021-09-30 23:08:40 +0200

    switch

--- functions.js
+++ functions.js
@@ -2,9 +2,24 @@ function test(){
     var selectElem = document.getElementById("card-select")
     choice = selectElem.selectedIndex
     value = selectElem.options[choice].value
-    console.log(value)
 
-    var maRegExp = new RegExp("^abc$", "i");
+    switch(value){
+        case 'visa':
+            var maRegExp = new RegExp("^visa$", "i");
+        break;
+        case 'mastercard':
+            var maRegExp = new RegExp("^mastercard$", "i");
+        break;
+        case 'amex':
+            var maRegExp = new RegExp("^amex$", "i");
+        break;
+        case 'dinnerclub':
+            var maRegExp = new RegExp("^dinnerclub$", "i");
+        break;
+        case 'discover':
+            var maRegExp = new RegExp("^discover$", "i");
+        break;
+    }
 
     var form = document.getElementById("formulaire").value
 
