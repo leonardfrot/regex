e396181cb6a38cfb38f45fa43e8b7d2b2c19c848
leonard.frot <leonard.frote@he-arc.ch>
2021-10-01 16:44:14 +0200

    finish

--- functions.js
+++ functions.js
@@ -5,19 +5,19 @@ function test(){
 
     switch(value){
         case 'visa':
-            var maRegExp = new RegExp("^visa$", "i");
+            var maRegExp = new RegExp("^4[1-9]{12}([1-9]{3})*$", "i");
         break;
         case 'mastercard':
-            var maRegExp = new RegExp("^mastercard$", "i");
+            var maRegExp = new RegExp("^5[1-5][1-9]{14}$", "i");
         break;
         case 'amex':
-            var maRegExp = new RegExp("^amex$", "i");
+            var maRegExp = new RegExp("^3(4|7)[1-9]{13}$", "i");
         break;
         case 'dinnerclub':
-            var maRegExp = new RegExp("^dinnerclub$", "i");
+            var maRegExp = new RegExp("^3(?:0[0-5]|[68][0-9])[0-9]{11}", "i");
         break;
         case 'discover':
-            var maRegExp = new RegExp("^discover$", "i");
+            var maRegExp = new RegExp("^6001[1-9]{12}$", "i");
         break;
     }
 
@@ -27,8 +27,10 @@ function test(){
         console.log("ok")
         }
         else{
-        console.log("pas ok");
+        console.log("pas ok")
+        console.log(maRegExp.exec())
         }
     
 }
+//|  ^6001[1-9]{12}$
 
